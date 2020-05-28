0c3034b3a44eff5b53dda51869ff7de4c88faca0
Poiuh547 <61447549+Poiuh547@users.noreply.github.com>
2020-05-28 03:32:16 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,36 @@
 
+window.addEventListener('load', ()=>{
+
+	var evento1 = document.getElementById("evento1");
+	var evento2 = document.getElementById("evento2");
+	var ocultar1 = document.querySelector("#parrafoinf");
+	var ocultar2 = document.querySelector("#label1");
+
+	
+
+	evento1.addEventListener('click', ()=>{
+
+
+		ocultar1.animete()
+		/*ocultar1.style.transition ="all 2s";
+		ocultar2.style.transition= "all 2s";
+		ocultar1.style.display = "none";
+		ocultar2.style.display = "none";*/
+		
+		
+		
+	});
+
+
+	evento2.addEventListener('click', ()=>{
+
+
+		ocultar1.style.display = "block";
+		ocultar2.style.display = "block";
+		
+		
+	});
+
+
+
+});
